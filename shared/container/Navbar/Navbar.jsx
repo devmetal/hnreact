bcd1e962c64622667b0d5bc10dfa07cd27629812
Metál Ádám <devmetal91@gmail.com>
2016-11-01 16:15:04 +0100

    add login button and a sidenav

--- shared/container/Navbar/Navbar.jsx
+++ shared/container/Navbar/Navbar.jsx
@@ -1,17 +1,26 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+import SideNavButton from '../../components/SideNavButton/SideNavButton';
+
 class Navbar extends Component {
   constructor(props, context) {
     super(props, context);
   }
-  
+
   render() {
     return (
       <nav>
         <div className="nav-wrapper">
           <div className="container">
             <a href="#" className="brand-logo">HackerNews Reader</a>
+            <SideNavButton />
+            <ul id="nav-mobile" className="right hide-on-med-and-down">
+              <li><a href="#">Login</a></li>
+            </ul>
+            <ul className="side-nav" id="mobile-demo">
+              <li><a href="#">Login</a></li>
+            </ul>
           </div>
         </div>
       </nav>
