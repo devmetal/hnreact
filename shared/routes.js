4f633db381cd43fd62846efa426338475e383134
Metál Ádám <devmetal91@gmail.com>
2016-12-22 23:01:30 +0100

    Created favorites components

--- shared/routes.js
+++ shared/routes.js
@@ -2,10 +2,12 @@ import { Route, IndexRoute } from 'react-router';
 import React from 'react';
 import App from './container/App';
 import Reader from './container/Reader/Reader';
+import Favorites from './container/Favorites/Favorites';
 
 const routes = (
   <Route path="/" component={App} >
     <IndexRoute component={Reader} />
+    <Route path="/favorites" component={Favorites} />
   </Route>
 );
 
